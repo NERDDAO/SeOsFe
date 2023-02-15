41be5e503763949b87a7077f2ff53a2e491eb41b
At0x <at0x@pop-os.localdomain>
2023-02-15 16:04:54 -0500

    added graph implementation

--- packages/nextjs/logic/factoryOfFactories.js
+++
@@ -1,5 +0,0 @@
-import { blockchainCall, web3Utils, sendAsync, getNetworkElement, numberToString, VOID_ETHEREUM_ADDRESS, formatLink } from "@ethereansos/interfaces-core"
-
-export function allFactories({}, factoryOfFactories) {
-    return blockchainCall(factoryOfFactories.methods.all)
-}
